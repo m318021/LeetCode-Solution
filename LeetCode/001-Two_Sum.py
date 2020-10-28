40da106ed67a5b2d1aa17138e68e36f6393ed8e6
m318021 <m318021@gmail.com>
2020-10-28 18:15:38 +0800

    add LeetCode Folder and script

---
+++ LeetCode/001-Two_Sum.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict_map = {}
+        
+        for i in range(len(nums)):
+            value = target - nums[i] 
+            if value in dict_map:
+                return [dict_map[value], i]
+            else:
+                dict_map[nums[i]] = i
+
+if __name__ == '__main__':
+
+	nums = [ 2, 7, 11, 15 ]
+	target = 9
+	result = Solution().twoSum(nums, target)
+	print(result)
