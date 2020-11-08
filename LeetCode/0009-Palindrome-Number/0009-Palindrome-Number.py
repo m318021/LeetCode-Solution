07ebeb780fe95acebbf3ecbb24364de64790c571
m318021 <m318021@gmail.com>
2020-11-09 04:06:26 +0800

    add 0008, 0009

---
+++ LeetCode/0009-Palindrome-Number/0009-Palindrome-Number.py
@@ -0,0 +1,45 @@
+# 8. String to Integer (atoi) - Medium 
+# https://leetcode.com/problems/string-to-integer-atoi/
+
+from typing import List
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+
+        if x < 0:
+            return False
+
+
+        reverse_value = 0
+        n = x
+        while n != 0:
+            reverse_value = reverse_value *10 + n%10
+
+            n = n//10
+
+        return reverse_value == x
+
+
+
+if __name__ == '__main__':
+
+    input_1 = 121
+    result1 = Solution().isPalindrome(input_1)
+    print("Input: str = {} \nOutput = {}\nExpect = True".format(input_1, result1))
+
+    input_2= -121
+    result2 = Solution().isPalindrome(input_2)
+    print("Input: str = {} \nOutput = {}\nExpect = False".format(input_2, result2))
+
+    input_3 = 10
+    result3 = Solution().isPalindrome(input_3)
+    print("Input: str = {} \nOutput = {}\nExpect = False".format(input_3, result3))
+
+    input_4 = -101
+    result4 = Solution().isPalindrome(input_4)
+    print("Input: str = {} \nOutput = {}\nExpect = False".format(input_4, result4))
+
+    input_5 = 8
+    result4 = Solution().isPalindrome(input_5)
+    print("Input: str = {} \nOutput = {}\nExpect = True".format(input_5, result4))
