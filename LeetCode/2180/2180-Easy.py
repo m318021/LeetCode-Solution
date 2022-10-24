0c39126fe6a0d63ae54318a4fb5c4cb62ae336c7
Sheldon Hunag <m318021@gmail.com>
2022-10-25 01:15:37 +0800

    add 0058, 0202, 0258, 1945, 2180

---
+++ LeetCode/2180/2180-Easy.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/count-integers-with-even-digit-sum/
+
+class Solution:
+    def countEven(self, num: int) -> int:
+
+        count = 0
+        for i in range(1,num+1):
+            str_num = str(i)
+            temp = 0
+            for char in str_num:
+                temp += int(char)
+            if temp%2==0:
+                count += 1
+            str_num = str(temp)
+        return count
+
+
+
+
+if __name__ == '__main__':
+    num = 4
+    result = Solution().countEven(num)
+    print(result)
+
+    num = 30
+    result = Solution().countEven(num)
+    print(result)
