d2dce0b6ad8ff1e2a96f79b97ca24c31e8bcb7b5
Sheldon Hunag <m318021@gmail.com>
2022-10-31 04:16:47 +0800

    1. add 1030, 2. update general.readme, 3. update README.md 4. update 2404

--- utility/general_readme.py
+++ utility/general_readme.py
@@ -9,6 +9,7 @@ from urllib.parse import urljoin
 root_path = pathlib.Path(__file__).parent.parent.resolve()
 
 folder_name = "LeetCode"
+README_MD = "README.md"
 leetCode_yaml_path = "utility/leetCode.yaml"
 default_file_text = "from typing import List\nif __name__ == '__main__': \n    #input\n    #output"
 language = "Python3"
@@ -127,60 +128,59 @@ def generate_readme_md_by_yaml(yaml_file_path = leetCode_yaml_path, open_file = 
             str_case_id = str(case_id)
 
         add_line = func_print_case_info(str_case_id)
-        # result = func_generate_case_info(str_case_id)
-        #
-        # add_line = "| {} | [{}]({}) | [{}](./{}) | {} | ".format(str_case_id,
-        #                                                       result["title"],
-        #                                                       result["url"],
-        #                                                       language,
-        #                                                       result["case_path"],
-        #                                                       result["difficulty"]
-        #                                                       )
-        #
-        # if open_file:
-        #     if os.path.isdir(result["folder_path"]) == False:
-        #         os.mkdir(result["folder_path"])
-        #         print("Create Folder : {}".format(result["folder_path"]))
-        #
-        #         f = open(result["case_path"], "w+")
-        #         f.write(default_file_text)
-        #         f.close()
-        #         print("Create File : {}".format(result["case_path"]))
-        #
-        #     else:
-        #         print("FIle : \"{}\" exist".format(result["case_path"]))
-
         readme_md.append(add_line)
 
     return readme_md
 
-def generate_readme_md_info():
+def generate_readme_md_case_info():
     case_list = os.listdir(path=folder_name)
 
     result = []
     for folder in case_list:
+
         if folder != ".DS_Store":
             split_folder_name = folder.split("-")
-
+            print("-", end='')
             add_line = func_print_case_info(split_folder_name[0], False)
-
             result.append(add_line)
+
+    print("Done")
 
     return result
 
+
+def write_readme_md_file(readme_data):
+
+
+    summary = "LeetCode\n========\n\n|  | ID | Title | python3                                   | Difficulty |\n| -- | ----- | ------------------------------------------- | ---------- | "
+    count = 1
+    for item in readme_data:
+        summary = summary + "\n" +"|  " + str(count) +item
+        count = count+1
+
+    f = open(README_MD, "w")
+    f.write(summary)
+    f.close()
+
+
+
+
 if __name__ == '__main__':
 
-    # read_md_info = generate_readme_md_by_yaml(open_file=True)
-    # read_md_info.sort()
-    # print("\nREADME.md ===============================\n")
-    # print_result(read_md_info)
-    # print("\n ===============================\n")
-    # print("add {} cases".format(len(read_md_info)))
-
-    result = generate_readme_md_info()
+    read_md_info = generate_readme_md_by_yaml(open_file=True)
+    read_md_info.sort()
+    print("\nREADME.md ===============================\n")
+    print_result(read_md_info)
+    print("\n ===============================\n")
+    print("add {} cases".format(len(read_md_info)))
+    print("\n\n ===============================\n\n")
+    result = generate_readme_md_case_info()
     result.sort()
-    print_result(result)
+    print("Upate READEME.md ")
+    write_readme_md_file(result)
     print("{} cases".format(len(result)))
+
+
 
 
 
